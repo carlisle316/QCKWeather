c07817a5a6c8190545f979581861434310867bbb
Chris Carlisle <ccarlis@gmail.com>
2016-11-05 17:59:26 -0500

    Committed GPS update

--- QCKWeather/app/src/main/java/com/carlisle/android/aca/qckweather/MainActivity.java
+++ QCKWeather/app/src/main/java/com/carlisle/android/aca/qckweather/MainActivity.java
@@ -1,7 +1,10 @@
 package com.carlisle.android.aca.qckweather;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -9,6 +12,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -23,6 +27,10 @@ import android.widget.TextView;
 
 import com.carlisle.android.aca.qckweather.model.BaseWeather;
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -45,6 +53,7 @@ public class MainActivity extends AppCompatActivity
     LocationManager mLocationManager;
 
     private String provider;
+    private String mCityName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,7 +79,30 @@ public class MainActivity extends AppCompatActivity
 
         Criteria criteria = new Criteria();
         provider = mLocationManager.getBestProvider(criteria, false);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         Location location = mLocationManager.getLastKnownLocation(provider);
+
+        if (location != null) {
+            double lat = location.getLatitude();
+            double lng = location.getLongitude();
+
+            //call method
+            try {
+                mCityName = getCity(lat, lng);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -97,14 +129,14 @@ public class MainActivity extends AppCompatActivity
 
         WeatherApiService service = retrofit.create(WeatherApiService.class);
 
-        Call<BaseWeather> call = service.getWeather("Conway");
+        Call<BaseWeather> call = service.getWeather(mCityName);
         call.enqueue(new Callback<BaseWeather>() {
             @Override
             public void onResponse(Call<BaseWeather> call, Response<BaseWeather> response) {
                 mWeather = response.body();
                 Log.i("Weather: ", mWeather.toString());
 
-                txtLocation.setText(mWeather.getName());
+                txtLocation.setText(mCityName);
                 txtTemp.setText(mWeather.getMain().getTemp().toString());
                 Picasso.with(getApplicationContext())
                         .load(IMG_ADDRESS + mWeather.getWeather().get(0).getIcon() + ".png")
@@ -128,6 +160,39 @@ public class MainActivity extends AppCompatActivity
                 .placeholder(R.color.colorAccent)
                 .into(imgIcon);
                 */
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLocationManager.requestLocationUpdates(provider, 500, 1, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLocationManager.removeUpdates(this);
     }
 
     @Override
@@ -184,6 +249,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onLocationChanged(Location location) {
 
+
+
     }
 
     @Override
@@ -199,5 +266,15 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onProviderDisabled(String provider) {
 
+    }
+
+    public String getCity(double lat, double lng) throws IOException {
+        Geocoder gcd = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = gcd.getFromLocation(lat, lng, 1);
+        if (addresses.size() > 0) {
+            System.out.println(addresses.get(0).getLocality());
+            mCityName = addresses.get(0).getLocality();
+        }
+        return mCityName;
     }
 }
